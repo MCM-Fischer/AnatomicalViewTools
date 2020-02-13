0978d72c81567d2c06ce4b25ac7a58c2fbf2d583
MCMF <>
2020-02-13 20:09:26 +0100

    first commit

---
+++ anatomicalCoordinateSystemTFM.m
@@ -0,0 +1,121 @@
+function TFM = anatomicalCoordinateSystemTFM(origin, target)
+%ANATOMICALCOORDINATESYSTEMTFM creates the transform from the origin to the
+%target anatomical coordinate system
+% 
+%   The three letters describe the positive XYZ direction:
+%       
+%   Example: 'RIA'    
+%     ________________________________________________________
+%     |    Axes    |      X      |      Y      |      Z      |
+%     |  Positive  |   [R]ight   |  [I]nferior |  [A]nterior |
+%     |  Negative  |    Left     |   Superior  |  Posterior  |
+%     |______________________________________________________|
+%
+% AUTHOR: Maximilian C. M. Fischer
+% COPYRIGHT (C) 2020 Maximilian C. M. Fischer
+% LICENSE: 
+%
+
+p = inputParser;
+validStrings={...
+    'RAS','RSP','RPI','RIA',...
+    'ALS','ASR','ARI','AIL',...
+    'LPS','LSA','LAI','LIP',...
+    'PRS','PSL','PLI','PIR',...
+    'IAR','IRP','IPL','ILS',...
+    'SAL','SLP','SPR','SRA'};
+addRequired(p,'origin',@(x) any(validatestring(upper(x),validStrings)))
+addRequired(p,'target',@(x) any(validatestring(upper(x),validStrings)));
+
+parse(p,origin,target);
+origin = upper(p.Results.origin);
+target = upper(p.Results.target);
+
+TFM=convertRAS(target)*convertRAS(origin)';
+
+end
+
+function TFM=convertRAS(target)
+
+switch target
+%% R
+    case 'RAS'
+        % Convert 'RAS' to 'RAS'
+        TFM = eye(4);
+    case 'RSP'
+        % Convert 'RAS' to 'RSP'
+        TFM = createRotationOx(-pi/2);
+    case 'RPI'
+        % Convert 'RAS' to 'RPI'
+        TFM = createRotationOx(   pi);
+    case 'RIA'
+        % Convert 'RAS' to 'RIA'
+        TFM = createRotationOx( pi/2);
+%% A
+    case 'ALS'
+        % Convert 'RAS' to 'ALS'
+        TFM =                         createRotationOz(-pi/2);
+    case 'ASR'
+        % Convert 'RAS' to 'ASR'
+        TFM = createRotationOx(-pi/2)*createRotationOz(-pi/2);
+    case 'ARI'
+        % Convert 'RAS' to 'ARI'
+        TFM = createRotationOx(   pi)*createRotationOz(-pi/2);
+    case 'AIL'
+        % Convert 'RAS' to 'AIL'
+        TFM = createRotationOx( pi/2)*createRotationOz(-pi/2);
+%% L
+    case 'LPS'
+        % Convert 'RAS' to 'LPS'
+        TFM =                         createRotationOz(pi);
+    case 'LSA'
+        % Convert 'RAS' to 'LSA'
+        TFM = createRotationOx(-pi/2)*createRotationOz(pi);
+    case 'LAI'
+        % Convert 'RAS' to 'LAI'
+        TFM = createRotationOx(   pi)*createRotationOz(pi);
+    case 'LIP'
+        % Convert 'RAS' to 'LIP'
+        TFM = createRotationOx( pi/2)*createRotationOz(pi);
+%% P
+    case 'PRS'
+        % Convert 'RAS' to 'PRS'
+        TFM =                         createRotationOz(pi/2);
+    case 'PSL'
+        % Convert 'RAS' to 'PSL'
+        TFM = createRotationOx(-pi/2)*createRotationOz(pi/2);
+    case 'PLI'
+        % Convert 'RAS' to 'PLI'
+        TFM = createRotationOx(   pi)*createRotationOz(pi/2);
+    case 'PIR'
+        % Convert 'RAS' to 'PIR'
+        TFM = createRotationOx( pi/2)*createRotationOz(pi/2);
+%% I
+    case 'IAR'
+        % Convert 'RAS' to 'IAR'
+        TFM =                         createRotationOy(-pi/2);
+    case 'IRP'
+        % Convert 'RAS' to 'IRP'
+        TFM = createRotationOx(-pi/2)*createRotationOy(-pi/2);
+    case 'IPL'
+        % Convert 'RAS' to 'IPL'
+        TFM = createRotationOx(   pi)*createRotationOy(-pi/2);
+    case 'ILS'
+        % Convert 'RAS' to 'ILS'
+        TFM = createRotationOx( pi/2)*createRotationOy(-pi/2);
+%% S
+    case 'SAL'
+        % Convert 'RAS' to 'SAL'
+        TFM =                         createRotationOy( pi/2);
+    case 'SLP'
+        % Convert 'RAS' to 'SLP'
+        TFM = createRotationOx(-pi/2)*createRotationOy( pi/2);
+    case 'SPR'
+        % Convert 'RAS' to 'SPR'
+        TFM = createRotationOx(   pi)*createRotationOy( pi/2);
+    case 'SRA'
+        % Convert 'RAS' to 'SRA'
+        TFM = createRotationOx( pi/2)*createRotationOy( pi/2);
+end
+
+end
---
+++ anatomicalViewButtons_test.m
@@ -0,0 +1,99 @@
+clearvars; close all; opengl hardware
+
+load('femur') % in 'RAS'
+
+%% Test
+validStrings={...
+    'RAS','RSP','RPI','RIA',...
+    'ALS','ASR','ARI','AIL',...
+    'LPS','LSA','LAI','LIP',...
+    'PRS','PSL','PLI','PIR',...
+    'IAR','IRP','IPL','ILS',...
+    'SAL','SLP','SPR','SRA'};
+
+aCS1=validStrings{randi(24)};
+aCS2=validStrings{randi(24)};
+mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS',aCS1));
+mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM(aCS1, aCS2));
+mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM(aCS2,'RAS'));
+visualizeMeshes(mesh)
+anatomicalViewButtons('RAS')
+
+%% R
+%% Convert 'RAS' to 'RIA'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RAS')))
+% anatomicalViewButtons('RAS')
+%% Convert 'RAS' to 'RIA'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RIA')))
+% anatomicalViewButtons('RIA')
+%% Convert 'RAS' to 'RPI'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RPI')))
+% anatomicalViewButtons('RPI')
+%% Convert 'RAS' to 'RSP'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RSP')))
+% anatomicalViewButtons('RSP')
+%% A
+%% Convert 'RAS' to 'ALS'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ALS')))
+% anatomicalViewButtons('ALS')
+%% Convert 'RAS' to 'ASR'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ASR')))
+% anatomicalViewButtons('ASR')
+%% Convert 'RAS' to 'ARI'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ARI')))
+% anatomicalViewButtons('ARI')
+%% Convert 'RAS' to 'AIL'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','AIL')))
+% anatomicalViewButtons('AIL')
+%% L
+%% Convert 'RAS' to 'LPS'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LPS')))
+% anatomicalViewButtons('LPS')
+%% Convert 'RAS' to 'LSA'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LSA')))
+% anatomicalViewButtons('LSA')
+%% Convert 'RAS' to 'LAI'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LAI')))
+% anatomicalViewButtons('LAI')
+%% Convert 'RAS' to 'LIP'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LIP')))
+% anatomicalViewButtons('LIP')
+%% P
+%% Convert 'RAS' to 'PRS'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PRS')))
+% anatomicalViewButtons('PRS')
+%% Convert 'RAS' to 'PSL'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PSL')))
+% anatomicalViewButtons('PSL')
+%% Convert 'RAS' to 'PLI'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PLI')))
+% anatomicalViewButtons('PLI')
+%% Convert 'RAS' to 'PIR'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PIR')))
+% anatomicalViewButtons('PIR')
+%% I
+%% Convert 'RAS' to 'IAR'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IAR')))
+% anatomicalViewButtons('IAR')
+%% Convert 'RAS' to 'IRP'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IRP')))
+% anatomicalViewButtons('IRP')
+%% Convert 'RAS' to 'IPL'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IPL')))
+% anatomicalViewButtons('IPL')
+%% Convert 'RAS' to 'ILS'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ILS')))
+% anatomicalViewButtons('ILS')
+%% S
+%% Convert 'RAS' to 'SAL'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SAL')))
+% anatomicalViewButtons('SAL')
+%% Convert 'RAS' to 'SLP'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SLP')))
+% anatomicalViewButtons('SLP')
+%% Convert 'RAS' to 'SPR'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SPR')))
+% anatomicalViewButtons('SPR')
+%% Convert 'RAS' to 'SRA'
+% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SRA')))
+% anatomicalViewButtons('SRA')
