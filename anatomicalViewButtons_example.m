1d990cb3851b324c406d3702d5567fbdc4bc1229
MCMF <>
2020-02-16 16:10:18 +0100

    bug fix

--- anatomicalViewButtons_example.m
+++ anatomicalViewButtons_example.m
@@ -16,89 +16,89 @@ validStrings={...
 
 aCS1=validStrings{randi(24)};
 aCS2=validStrings{randi(24)};
-mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS',aCS1));
-mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM(aCS1, aCS2));
-mesh=transformPoint3d(mesh, anatomicalCoordinateSystemTFM(aCS2,'RAS'));
+mesh=transformPoint3d(mesh, anatomicalOrientationTFM('RAS',aCS1));
+mesh=transformPoint3d(mesh, anatomicalOrientationTFM(aCS1, aCS2));
+mesh=transformPoint3d(mesh, anatomicalOrientationTFM(aCS2,'RAS'));
 visualizeMeshes(mesh)
 anatomicalViewButtons('RAS')
 
 %% R
 %% Convert 'RAS' to 'RIA'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RAS')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','RAS')))
 % anatomicalViewButtons('RAS')
 %% Convert 'RAS' to 'RIA'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RIA')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','RIA')))
 % anatomicalViewButtons('RIA')
 %% Convert 'RAS' to 'RPI'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RPI')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','RPI')))
 % anatomicalViewButtons('RPI')
 %% Convert 'RAS' to 'RSP'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','RSP')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','RSP')))
 % anatomicalViewButtons('RSP')
 %% A
 %% Convert 'RAS' to 'ALS'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ALS')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','ALS')))
 % anatomicalViewButtons('ALS')
 %% Convert 'RAS' to 'ASR'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ASR')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','ASR')))
 % anatomicalViewButtons('ASR')
 %% Convert 'RAS' to 'ARI'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ARI')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','ARI')))
 % anatomicalViewButtons('ARI')
 %% Convert 'RAS' to 'AIL'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','AIL')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','AIL')))
 % anatomicalViewButtons('AIL')
 %% L
 %% Convert 'RAS' to 'LPS'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LPS')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','LPS')))
 % anatomicalViewButtons('LPS')
 %% Convert 'RAS' to 'LSA'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LSA')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','LSA')))
 % anatomicalViewButtons('LSA')
 %% Convert 'RAS' to 'LAI'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LAI')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','LAI')))
 % anatomicalViewButtons('LAI')
 %% Convert 'RAS' to 'LIP'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','LIP')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','LIP')))
 % anatomicalViewButtons('LIP')
 %% P
 %% Convert 'RAS' to 'PRS'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PRS')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','PRS')))
 % anatomicalViewButtons('PRS')
 %% Convert 'RAS' to 'PSL'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PSL')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','PSL')))
 % anatomicalViewButtons('PSL')
 %% Convert 'RAS' to 'PLI'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PLI')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','PLI')))
 % anatomicalViewButtons('PLI')
 %% Convert 'RAS' to 'PIR'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','PIR')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','PIR')))
 % anatomicalViewButtons('PIR')
 %% I
 %% Convert 'RAS' to 'IAR'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IAR')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','IAR')))
 % anatomicalViewButtons('IAR')
 %% Convert 'RAS' to 'IRP'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IRP')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','IRP')))
 % anatomicalViewButtons('IRP')
 %% Convert 'RAS' to 'IPL'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','IPL')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','IPL')))
 % anatomicalViewButtons('IPL')
 %% Convert 'RAS' to 'ILS'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','ILS')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','ILS')))
 % anatomicalViewButtons('ILS')
 %% S
 %% Convert 'RAS' to 'SAL'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SAL')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','SAL')))
 % anatomicalViewButtons('SAL')
 %% Convert 'RAS' to 'SLP'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SLP')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','SLP')))
 % anatomicalViewButtons('SLP')
 %% Convert 'RAS' to 'SPR'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SPR')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','SPR')))
 % anatomicalViewButtons('SPR')
 %% Convert 'RAS' to 'SRA'
-% visualizeMeshes(transformPoint3d(mesh, anatomicalCoordinateSystemTFM('RAS','SRA')))
+% visualizeMeshes(transformPoint3d(mesh, anatomicalOrientationTFM('RAS','SRA')))
 % anatomicalViewButtons('SRA')
 
 
