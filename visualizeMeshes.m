474501e622912c9e0337636f9de9ac4f3d15a847
MCMF <>
2020-07-26 20:30:09 +0200

    optional axes and figure input added

--- visualizeMeshes.m
+++ visualizeMeshes.m
@@ -1,4 +1,4 @@
-function varargout = visualizeMeshes(mesh, patchProps)
+function varargout = visualizeMeshes(mesh, patchProps, varargin)
 %VISUALIZEMESHES plots a struct array of meshes in a new figure
 %
 % AUTHOR: Maximilian C. M. Fischer
@@ -11,6 +11,21 @@ defPatchProps.FaceColor = [223, 206, 161]/255;
 defPatchProps.FaceAlpha = 1;
 defPatchProps.FaceLighting = 'gouraud';
 
+if isa(mesh, 'matlab.ui.Figure')
+    figHandle = mesh;
+    mesh = patchProps;
+    if ~isempty(varargin)
+        patchProps = varargin{1};
+    end
+elseif isa(mesh, 'matlab.graphics.axis.Axes')
+    axHandle = mesh;
+    figHandle = ancestor(axHandle,'figure');
+    mesh = patchProps;
+    if ~isempty(varargin)
+        patchProps = varargin{1};
+    end
+end
+    
 if nargin == 1
     patchProps = defPatchProps;
 else
@@ -31,26 +46,30 @@ if length(patchProps)==1
     patchProps=repmat(patchProps, length(mesh), 1);
 end
 
-% New figure
-MonitorsPos = get(0,'MonitorPositions');
-figHandle = figure('Units','pixels','renderer','opengl', 'Color', 'w');
-if     size(MonitorsPos,1) == 1
-    set(figHandle,'OuterPosition',[1 50 MonitorsPos(1,3)-1 MonitorsPos(1,4)-50]);
-elseif size(MonitorsPos,1) == 2
-    set(figHandle,'OuterPosition',[1+MonitorsPos(1,3) 50 MonitorsPos(2,3)-1 MonitorsPos(2,4)-50]);
+if ~exist('figHandle', 'var')
+    % New figure
+    MonitorsPos = get(0,'MonitorPositions');
+    figHandle = figure('Units','pixels','renderer','opengl', 'Color', 'w');
+    if     size(MonitorsPos,1) == 1
+        set(figHandle,'OuterPosition',[1 50 MonitorsPos(1,3)-1 MonitorsPos(1,4)-50]);
+    elseif size(MonitorsPos,1) == 2
+        set(figHandle,'OuterPosition',[1+MonitorsPos(1,3) 50 MonitorsPos(2,3)-1 MonitorsPos(2,4)-50]);
+    end
+    figHandle.WindowState = 'maximized';
 end
-figHandle.WindowState = 'maximized';
 
-axHandle = axes;
+if ~exist('axHandle', 'var')
+    axHandle = axes;
+end
 meshHandle=zeros(length(mesh),1);
 for i=1:length(mesh)
-    meshHandle(i) = patch(mesh(i), patchProps(i));
+    meshHandle(i) = patch(axHandle, mesh(i), patchProps(i));
 end
 
-H_Light(1) = light; light('Position', -1*(get(H_Light(1),'Position')));
+H_Light(1) = light(axHandle); light(axHandle, 'Position', -1*(get(H_Light(1),'Position')));
 % cameratoolbar('SetCoordSys','none')
-axis on; axis equal; hold on
-xlabel x; ylabel y; zlabel z;
+axis(axHandle, 'on'); axis(axHandle, 'equal'); hold(axHandle, 'on')
+xlabel(axHandle, 'x'); ylabel(axHandle, 'y'); zlabel(axHandle, 'z');
 
 if nargout > 0
     varargout{1} = meshHandle;
